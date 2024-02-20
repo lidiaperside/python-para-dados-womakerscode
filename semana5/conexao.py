33e7d583e805c124291180df2a82422864d134bd
lidiaperside <lidiaperside@gmail.com>
2024-02-20 20:28:52 -0300

    criando a tabela

--- semana5/conexao.py
+++ semana5/conexao.py
@@ -7,7 +7,9 @@ conexao = sqlite3.connect('bancodedados')
 #passando a conexão para uma nova variável
 cursor = conexao.cursor()
 
-cursor.execute('')
+#Criando a tabela
+cursor.execute('CREATE TABLE usuarios(id INT, nome VARCHAR(100), endereco VARCHAR(100), email VARCHAR(100));')
+#Caso queira criar uma nova tabela, basta comentar a tabela ja criada
 
 #indicando que serão enviadas novas informações
 conexao.commit()
