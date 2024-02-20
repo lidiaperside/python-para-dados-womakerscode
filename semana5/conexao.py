3be7abf7077f2cee9add405d1d4f159865c640cb
lidiaperside <lidiaperside@gmail.com>
2024-02-19 22:55:38 -0300

    conectando o banco de dados

---
+++ semana5/conexao.py
@@ -0,0 +1,16 @@
+#importando o BD
+import sqlite3
+
+#conexão com o arquivo que tem o mesmo nome criado no BD
+conexao = sqlite3.connect('banco')
+
+#passando a conexão para uma nova variável
+cursor = conexao.cursor()
+
+cursor.execute('')
+
+#indicando que serão enviadas novas informações
+conexao.commit()
+
+#finaliza para não gerar conflitos
+conexao.close
