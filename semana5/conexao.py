4401e1e0532bd36a47cb6085f1b1eb284efba97c
lidiaperside <lidiaperside@gmail.com>
2024-02-20 23:42:29 -0300

    ajustes

--- semana5/conexao.py
+++ semana5/conexao.py
@@ -2,7 +2,7 @@
 import sqlite3
 
 #conexão com o arquivo que tem o mesmo nome criado no BD
-conexao = sqlite3.connect('banco.db')
+conexao = sqlite3.connect('banco')
 
 #passando a conexão para uma nova variável
 cursor = conexao.cursor()
@@ -23,8 +23,13 @@ cursor = conexao.cursor()
 #cursor.execute('CREATE TABLE produtos(id INT, nome VARCHAR(100), endereco VARCHAR(100), email VARCHAR(100));')
 
 #Deletando uma tabela
-cursor.execute('DROP TABLE produtos')
+#cursor.execute('DROP TABLE produtos')
 
+#Inserindo dados
+cursor.execute("INSERT INTO usuario(id,nome,endereco,email,telefone) VALUES (1, 'Lidia', 'Sapé', 'lidia@lidia.com', 123)")
+cursor.execute("INSERT INTO usuario(id,nome,endereco,email,telefone) VALUES (2, 'Patricia', 'Sapé', 'patricia@patricia.com', 234)")
+cursor.execute("INSERT INTO usuario(id,nome,endereco,email,telefone) VALUES (3, 'Kao', 'Sapé', 'kao@kao.com', 345)")
+cursor.execute("INSERT INTO usuario(id,nome,endereco,email,telefone) VALUES (4, 'Jose Maria', 'João Pessoa', 'jose@maria.com', 456)")
 #indicando que serão enviadas novas informações
 conexao.commit()
 
